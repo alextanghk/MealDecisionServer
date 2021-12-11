a280b43bccc3e812c8504d151a83b755177e660e
alextanghk <alextang1030@gmail.com>
2021-12-11 19:27:44 +0000

    update init sql and remove bad code from index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const port = process.env.PORT !== undefined && Number.isInteger(Number(process.env.PORT)) ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log('');
+    console.log('Listening to port ?',port);
 });
